6048731f90daafa7b5429dbfbf38b5334d61cc40
merydaniel7 <merydaniel7@gmail.com>
2022-01-06 15:04:37 +0100

    Add: config for factory pattern

---
+++ src/main/java/com/tank/tankadminbackend/services/marketingcost/MarketingCostConfig.java
@@ -0,0 +1,9 @@
+package com.tank.tankadminbackend.services.marketingcost;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackageClasses = MarketingCostConfig.class)
+public class MarketingCostConfig {
+}
