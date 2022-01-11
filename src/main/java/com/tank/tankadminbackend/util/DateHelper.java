f5984a08984c1a36595aac676238a6ddcd6c0452
merydaniel7 <merydaniel7@gmail.com>
2022-01-11 13:49:16 +0100

    Add: get dates in reverse format

--- src/main/java/com/tank/tankadminbackend/util/DateHelper.java
+++ src/main/java/com/tank/tankadminbackend/util/DateHelper.java
@@ -25,4 +25,15 @@ public class DateHelper {
 
         return dates;
     }
+
+    public List<String> getDatesOfDaysReverse(int minusDaysStart, int minusDaysEnd) {
+        List<String> dates = new ArrayList<>();
+        LocalDate currentDate = LocalDate.now();
+        for (int i = minusDaysEnd; i > minusDaysStart - 1; i--) {
+            dates.add(currentDate.minusDays(i).format(DateTimeFormatter.ofPattern("yyyy.MM.dd")));
+        }
+
+        return dates;
+    }
+
 }
