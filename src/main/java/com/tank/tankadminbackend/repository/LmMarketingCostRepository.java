022a46a71916a49ad7c7b3525110312d1857789a
merydaniel7 <merydaniel7@gmail.com>
2022-01-11 14:45:02 +0100

    Add: function to get cost and profit by month

--- src/main/java/com/tank/tankadminbackend/repository/LmMarketingCostRepository.java
+++ src/main/java/com/tank/tankadminbackend/repository/LmMarketingCostRepository.java
@@ -4,6 +4,7 @@ import com.tank.tankadminbackend.models.lmdata.LmMarketingCost;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -11,4 +12,6 @@ public interface LmMarketingCostRepository extends JpaRepository<LmMarketingCost
     Boolean existsByDate(String date);
 
     Optional<LmMarketingCost> findByDate(String date);
+
+    List<LmMarketingCost> findByDateStartsWith(String month);
 }
--- src/main/java/com/tank/tankadminbackend/repository/LmProfitOnProductsRepository.java
+++ src/main/java/com/tank/tankadminbackend/repository/LmProfitOnProductsRepository.java
@@ -4,6 +4,7 @@ import com.tank.tankadminbackend.models.lmdata.LmProfitOnProducts;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -11,4 +12,6 @@ public interface LmProfitOnProductsRepository extends JpaRepository<LmProfitOnPr
     Boolean existsByDate(String date);
 
     Optional<LmProfitOnProducts> findByDate(String date);
+
+    List<LmProfitOnProducts> findByDateStartsWith(String month);
 }
