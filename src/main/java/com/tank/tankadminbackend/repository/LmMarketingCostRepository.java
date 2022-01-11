21747a13ec07fc704b159870a258e06a2720b2a8
merydaniel7 <merydaniel7@gmail.com>
2022-01-11 13:53:11 +0100

    Add: findByDate function

--- src/main/java/com/tank/tankadminbackend/repository/LmMarketingCostRepository.java
+++ src/main/java/com/tank/tankadminbackend/repository/LmMarketingCostRepository.java
@@ -4,7 +4,11 @@ import com.tank.tankadminbackend.models.lmdata.LmMarketingCost;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface LmMarketingCostRepository extends JpaRepository<LmMarketingCost, Long> {
     Boolean existsByDate(String date);
+
+    Optional<LmMarketingCost> findByDate(String date);
 }
--- src/main/java/com/tank/tankadminbackend/repository/LmProfitOnProductsRepository.java
+++ src/main/java/com/tank/tankadminbackend/repository/LmProfitOnProductsRepository.java
@@ -4,7 +4,11 @@ import com.tank.tankadminbackend.models.lmdata.LmProfitOnProducts;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface LmProfitOnProductsRepository extends JpaRepository<LmProfitOnProducts, Long> {
     Boolean existsByDate(String date);
+
+    Optional<LmProfitOnProducts> findByDate(String date);
 }
