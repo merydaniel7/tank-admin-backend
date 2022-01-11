89f107ee192600c23ab620dc73558d13709a77c9
merydaniel7 <merydaniel7@gmail.com>
2022-01-11 10:22:45 +0100

    Add: config file to factory pattern

---
+++ src/main/java/com/tank/tankadminbackend/services/unas/UnasServiceConfig.java
@@ -0,0 +1,9 @@
+package com.tank.tankadminbackend.services.unas;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackageClasses = UnasServiceConfig.class)
+public class UnasServiceConfig {
+}
