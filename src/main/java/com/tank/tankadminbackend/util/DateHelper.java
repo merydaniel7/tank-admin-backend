7dd914642ece3612e64ff9d08b14c483a673df31
merydaniel7 <merydaniel7@gmail.com>
2022-01-11 10:21:53 +0100

    Add: get dates from start day to end day

--- src/main/java/com/tank/tankadminbackend/util/DateHelper.java
+++ src/main/java/com/tank/tankadminbackend/util/DateHelper.java
@@ -3,6 +3,9 @@ package com.tank.tankadminbackend.util;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class DateHelper {
@@ -11,5 +14,15 @@ public class DateHelper {
         LocalDate currentDate = LocalDate.now();
         LocalDate yesterday = currentDate.minusDays(1);
         return yesterday.toString();
+    }
+
+    public List<String> getDatesOfDays(int minusDaysStart, int minusDaysEnd) {
+        List<String> dates = new ArrayList<>();
+        LocalDate currentDate = LocalDate.now();
+        for (int i = minusDaysStart; i < minusDaysEnd + 1; i++) {
+            dates.add(currentDate.minusDays(i).format(DateTimeFormatter.ofPattern("yyyy.MM.dd")));
+        }
+
+        return dates;
     }
 }
