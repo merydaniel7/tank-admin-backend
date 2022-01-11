271328088ca15a4545fdfc74d67c6a70ce27d323
merydaniel7 <merydaniel7@gmail.com>
2022-01-11 13:56:18 +0100

    Add: save marketing cost and profit to database

--- src/main/java/com/tank/tankadminbackend/services/update/DailyUpdateScheduler.java
+++ src/main/java/com/tank/tankadminbackend/services/update/DailyUpdateScheduler.java
@@ -1,27 +1,49 @@
 package com.tank.tankadminbackend.services.update;
 
+import com.tank.tankadminbackend.models.lmdata.LmMarketingCost;
+import com.tank.tankadminbackend.models.lmdata.LmProfitOnProducts;
+import com.tank.tankadminbackend.repository.LmMarketingCostRepository;
+import com.tank.tankadminbackend.repository.LmProfitOnProductsRepository;
 import com.tank.tankadminbackend.services.marketingcost.LegjobbMunkaruhaMarketingCostService;
+import com.tank.tankadminbackend.services.unas.DailyProfitOnProductsService;
+import com.tank.tankadminbackend.services.unas.UnasAuthTokenService;
 import com.tank.tankadminbackend.util.DateHelper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class DailyUpdateScheduler {
 
     private final LegjobbMunkaruhaMarketingCostService legjobbMunkaruhaMarketingCostService;
+    private final LmMarketingCostRepository lmMarketingCostRepository;
+    private final LmProfitOnProductsRepository lmProfitOnProductsRepository;
+    private final DailyProfitOnProductsService dailyProfitOnProductsService;
+    private final UnasAuthTokenService unasAuthTokenService;
     private final DateHelper dateHelper;
+    @Value("${unas.lm.api.key}")
+    String lmUnasApiKey;
+
 
     @Autowired
-    public DailyUpdateScheduler(LegjobbMunkaruhaMarketingCostService legjobbMunkaruhaMarketingCostService, DateHelper dateHelper) {
+    public DailyUpdateScheduler(LegjobbMunkaruhaMarketingCostService legjobbMunkaruhaMarketingCostService, DailyProfitOnProductsService dailyProfitOnProductsService, LmMarketingCostRepository lmMarketingCostRepository, LmProfitOnProductsRepository lmProfitOnProductsRepository, UnasAuthTokenService unasAuthTokenService, DateHelper dateHelper) {
         this.legjobbMunkaruhaMarketingCostService = legjobbMunkaruhaMarketingCostService;
+        this.dailyProfitOnProductsService = dailyProfitOnProductsService;
+        this.lmMarketingCostRepository = lmMarketingCostRepository;
+        this.lmProfitOnProductsRepository = lmProfitOnProductsRepository;
+        this.unasAuthTokenService = unasAuthTokenService;
         this.dateHelper = dateHelper;
     }
 
-    @Scheduled(cron = "${schedule.cron}")
+    @Scheduled(cron = "${schedule.cron.cost}")
+    @Transactional
     public void saveMarketingCost() throws IOException, InterruptedException, GeneralSecurityException {
         String date = dateHelper.getYesterdayString();
         float sumAdCost = 0;
@@ -37,6 +59,52 @@ public class DailyUpdateScheduler {
         float googleAdCost = legjobbMunkaruhaMarketingCostService.getGoogleAdCost(date);
         System.out.println("Google: " + googleAdCost);
         sumAdCost += googleAdCost;
-        System.out.println(sumAdCost);
+        System.out.println("Sum of ad cost - "+ date + " : " + sumAdCost);
+
+        if (!lmMarketingCostRepository.existsByDate(date)) {
+            LmMarketingCost lmMarketingCost = new LmMarketingCost();
+            lmMarketingCost.setArgep(argepAdCost);
+            lmMarketingCost.setArukereso(arukeresoAdCost);
+            lmMarketingCost.setFacebook(facebookAdCost);
+            lmMarketingCost.setGoogle(googleAdCost);
+            lmMarketingCost.setSum(sumAdCost);
+            lmMarketingCost.setDate(date);
+            lmMarketingCostRepository.save(lmMarketingCost);
+        } else {
+            Optional<LmMarketingCost> costOptional = lmMarketingCostRepository.findByDate(date);
+            float finalSumAdCost = sumAdCost;
+            costOptional.ifPresent(lmMarketingCost -> {
+                lmMarketingCost.setArgep(argepAdCost);
+                lmMarketingCost.setArukereso(arukeresoAdCost);
+                lmMarketingCost.setFacebook(facebookAdCost);
+                lmMarketingCost.setGoogle(googleAdCost);
+                lmMarketingCost.setSum(finalSumAdCost);
+            });
+        }
+    }
+
+
+    @Scheduled(cron = "${schedule.cron.profit}")
+    @Transactional
+    public void saveProfitOnProducts() throws IOException, InterruptedException {
+        List<String> dates = dateHelper.getDatesOfDaysReverse(1, 30);
+        String token = unasAuthTokenService.getAuthToken(lmUnasApiKey);
+
+        for (String date : dates) {
+            float profit = dailyProfitOnProductsService.getDailyProfitOnProducts(token, date);
+            System.out.println("Profit - "+ date + " : " + profit);
+            if (!lmProfitOnProductsRepository.existsByDate(date)) {
+                LmProfitOnProducts profitOnProducts = new LmProfitOnProducts();
+                profitOnProducts.setProfit(profit);
+                profitOnProducts.setDate(date);
+                lmProfitOnProductsRepository.save(profitOnProducts);
+            } else {
+                Optional<LmProfitOnProducts> profitOptional = lmProfitOnProductsRepository.findByDate(date);
+                profitOptional.ifPresent(profitOnProducts -> {
+                    profitOnProducts.setProfit(profit);
+                });
+            }
+            Thread.sleep(2000);
+        }
     }
 }
