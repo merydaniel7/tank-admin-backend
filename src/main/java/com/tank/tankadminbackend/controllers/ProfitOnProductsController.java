2e91b251235ce7bba0288f4db38bb33f14ac6ae3
merydaniel7 <merydaniel7@gmail.com>
2022-01-14 14:02:13 +0100

    Add: created service, removed repository from controller

--- src/main/java/com/tank/tankadminbackend/controllers/ProfitOnProductsController.java
+++ src/main/java/com/tank/tankadminbackend/controllers/ProfitOnProductsController.java
@@ -1,8 +1,7 @@
 package com.tank.tankadminbackend.controllers;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.tank.tankadminbackend.repository.LmProfitOnProductsRepository;
-import com.tank.tankadminbackend.services.unas.DailyProfitOnProductsService;
+import com.tank.tankadminbackend.services.ProfitOnProductsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,13 +12,10 @@ import javax.validation.Valid;
 @RequestMapping("/api/profit-on-products")
 public class ProfitOnProductsController {
     @Autowired
-    DailyProfitOnProductsService dailyProfitOnProductsService;
-
-    @Autowired
-    LmProfitOnProductsRepository lmProfitOnProductsRepository;
+    ProfitOnProductsService profitOnProductsService;
 
     @PostMapping("/lm")
     public String getProfitOnProducts(@Valid @RequestBody String month) throws JsonProcessingException {
-        return dailyProfitOnProductsService.getProfitOnProductsByMonth(month, lmProfitOnProductsRepository);
+        return profitOnProductsService.getLegjobbMunkaruhaProfitOnProductsByMonth(month);
     }
 }
---
+++ src/main/java/com/tank/tankadminbackend/services/ProfitOnProductsService.java
@@ -0,0 +1,34 @@
+package com.tank.tankadminbackend.services;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.tank.tankadminbackend.models.api.ActualMonth;
+import com.tank.tankadminbackend.models.lmdata.LmProfitOnProducts;
+import com.tank.tankadminbackend.repository.LmProfitOnProductsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class ProfitOnProductsService {
+    @Autowired
+    LmProfitOnProductsRepository lmProfitOnProductsRepository;
+
+    public String getLegjobbMunkaruhaProfitOnProductsByMonth(String month) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        ActualMonth actualMonth = mapper.readValue(month, ActualMonth.class);
+        List<LmProfitOnProducts> profitOnProducts = lmProfitOnProductsRepository.findByDateStartsWith(actualMonth.getMonth());
+        List<Map<String, Float>> profits = new ArrayList<>();
+        for (LmProfitOnProducts lmProfitOnProduct : profitOnProducts) {
+            Map<String, Float> dayProfit = new HashMap<>();
+            dayProfit.put(lmProfitOnProduct.getDate(), lmProfitOnProduct.getProfit());
+            profits.add(dayProfit);
+        }
+
+        return new ObjectMapper().writeValueAsString(profits);
+    }
+}
