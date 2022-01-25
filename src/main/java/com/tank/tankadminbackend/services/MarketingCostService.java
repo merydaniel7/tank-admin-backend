88944842d26ddb2d312b6eb72cc7da4bcec2ce38
merydaniel7 <merydaniel7@gmail.com>
2022-01-25 09:04:18 +0100

    Add: restructure data to get cost by channels too.

--- src/main/java/com/tank/tankadminbackend/services/MarketingCostService.java
+++ src/main/java/com/tank/tankadminbackend/services/MarketingCostService.java
@@ -21,13 +21,40 @@ public class MarketingCostService {
     public String getLegjobbMunkaruhaMarketingCostByMonth(String month) throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
         ActualMonth actualMonth = mapper.readValue(month, ActualMonth.class);
-        List<LmMarketingCost> lmMarketingCosts = lmMarketingCostRepository.findByDateStartsWith(actualMonth.getMonth());
+        List<LmMarketingCost> lmMarketingCosts = lmMarketingCostRepository.findByDateStartsWithOrderById(actualMonth.getMonth());
         List<Map<String, Float>> costs = new ArrayList<>();
+        List<Map<String, Float>> costsArgep = new ArrayList<>();
+        List<Map<String, Float>> costsArukereso = new ArrayList<>();
+        List<Map<String, Float>> costsFacebook = new ArrayList<>();
+        List<Map<String, Float>> costsGoogle = new ArrayList<>();
         for (LmMarketingCost marketingCost : lmMarketingCosts) {
             Map<String, Float> dayWithCost = new HashMap<>();
+            Map<String, Float> dayWithArgepCost = new HashMap<>();
+            Map<String, Float> dayWithArukeresoCost = new HashMap<>();
+            Map<String, Float> dayWithFacebookCost = new HashMap<>();
+            Map<String, Float> dayWithGoogleCost = new HashMap<>();
             dayWithCost.put(marketingCost.getDate(), marketingCost.getSum());
+            dayWithArgepCost.put(marketingCost.getDate(), marketingCost.getArgep());
+            dayWithArukeresoCost.put(marketingCost.getDate(), marketingCost.getArukereso());
+            dayWithFacebookCost.put(marketingCost.getDate(), marketingCost.getFacebook());
+            dayWithGoogleCost.put(marketingCost.getDate(), marketingCost.getGoogle());
             costs.add(dayWithCost);
+            costsArgep.add(dayWithArgepCost);
+            costsArukereso.add(dayWithArukeresoCost);
+            costsFacebook.add(dayWithFacebookCost);
+            costsGoogle.add(dayWithGoogleCost);
         }
-        return new ObjectMapper().writeValueAsString(costs);
+        Map<String, List<Map<String, Float>>> allCost = new HashMap<>();
+        allCost.put("sum", costs);
+        allCost.put("argep", costsArgep);
+        allCost.put("arukereso", costsArukereso);
+        allCost.put("facebook", costsFacebook);
+        allCost.put("google", costsGoogle);
+        return new ObjectMapper().writeValueAsString(allCost);
+    }
+
+    public String getMonths() throws JsonProcessingException {
+        List<String> months = lmMarketingCostRepository.findMonths();
+        return new ObjectMapper().writeValueAsString(months);
     }
 }
